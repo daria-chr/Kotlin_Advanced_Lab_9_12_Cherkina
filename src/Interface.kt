a088309a6d6deb7488e30b378fc8d9908ea5c9ac
daria-chr <cherkinadar@gmail.com>
2026-01-14 16:17:46 +0300

    Add interface

---
+++ src/Interface.kt
@@ -0,0 +1,86 @@
+interface Movable{
+    var speed: Int
+    val model: String
+    val number: String
+    fun move()
+    fun stop(){
+        println("останавливаемся...")
+    }
+}
+
+
+class Car(override val model: String, override val number: String):Movable{
+
+    override var speed: Int=60
+
+    override fun move() {
+        println("едеи на машине")
+    }
+
+}
+
+class AirCraft(override val model: String, override val number: String):Movable{
+    override var speed: Int=600
+
+    override fun move() {
+        println("Летим на самолете")
+    }
+
+    override fun stop() {
+        println("приземляемся...")
+    }
+}
+
+fun travel(obj:Movable)=obj.move()
+
+interface Worker{
+    fun work()
+}
+
+interface  Student{
+    fun study()
+}
+
+class WorkingStudent(val name: String):Worker,Student {
+    override fun work() {
+        println("$name работает")
+    }
+
+    override fun study() {
+        println("$name Учится")
+    }
+
+    interface VideoPlayable {
+        fun play()=println("Play video")
+    }
+
+    interface AudioPlayable {
+        fun play()=println("Play audio")
+
+    }
+
+    class MediaPlayer : VideoPlayable, AudioPlayable {
+        override fun play() {
+            println("Start playing")
+            super<VideoPlayable>.play()
+            super<AudioPlayable>.play()
+        }
+    }
+
+    fun main() {
+        val player = MediaPlayer()
+        player.play()
+
+        val car = Car("Lada", "134lad")
+        val airCraft = AirCraft("Boeing", "243")
+    }
+}
+//        val artem = WorkingStudent("Артем")
+
+//    val car=Car()
+//    val  aircraft=AirCraft()
+//    travel(obj = car)
+//    travel(obj = aircraft)
+//    car.move()
+//    aircraft.move()
+
--- src/Test.kt
+++ src/Test.kt
@@ -4,18 +4,31 @@ var age: Int = 18
             field=value
     }
 
+//abstract class Figure{
+//    abstract fun perimeter(): Float
+//    abstract fun area(): Float
+//}
+//class Rectangle(val width: Float,
+//    val height: Float) :Figure() {
+//    override fun perimeter(): Float {
+//        return 2*(width+height)
+//    }
+//}
+
 fun main(){
  //   println(age)
     age=55
     println(age)
     age=-43
     println(age)
-    val sword=Item(1,"Sword",1)
-    val bettersword=sword.copy(quantity = 2, name = "Better Sword")
-    println(sword.toString())
-    println(bettersword.toString())
-    val (id,name, quantity)=bettersword
-    println("$id | $name | $quantity")
+//    val sword=Item(1,"Sword",1)
+//    val bettersword=sword.copy(quantity = 2, name = "Better Sword")
+//    println(sword.toString())
+//    println(bettersword.toString())
+//    val (id,name, quantity)=bettersword
+//    println("$id | $name | $quantity")
+    val denis:Human=Person("Denis",23)
+    val anton:Person=Person("Anton",20)
 }
 
 data class Item(
@@ -25,5 +38,17 @@ data class Item(
 ){
     override fun toString(): String {
         return "ID: $id | name: $name |  quantity: $quantity"
+    }
+}
+
+abstract class Human(val name: String){
+    abstract var age: Int
+    abstract fun hello()
+}
+class Person(name: String,override var age: Int=0):Human(name){
+//    override var age: Int=0
+
+    override fun hello() {
+        println("My name is $name")
     }
 }
